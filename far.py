351cd805f138db50fd8e163cb4cbead31a21ca4e
Nucleocherry <152157637+Nucleocherry@users.noreply.github.com>
2025-11-08 20:02:15 +0100

    Change subprocess argument from '--headed' to '--xvfb'

--- far.py
+++ far.py
@@ -4,5 +4,5 @@ import sys
 
 def execute_it():
 	# Lancer generate_img.py en passant le prompt via une variable d'environnement
-	subprocess.run([sys.executable, "generate_img.py", "--headed"])
+	subprocess.run([sys.executable, "generate_img.py", "--xvfb"])
 
